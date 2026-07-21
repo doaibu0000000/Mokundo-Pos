8e67d415b4b21ff3d50c4911dd174d325935955a
doaibu0000000 <doa.ibu0000000@gmail.com>
2026-07-21 08:56:00 +0700

    Update PWA manifest purposes to force Android to use correct splash screen icon

--- vite.config.ts
+++ vite.config.ts
@@ -55,13 +55,13 @@ export default defineConfig({
             src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           },
           {
             src: 'pwa-512x512-maskable.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           },
           {
             src: 'splash-icon.png',
